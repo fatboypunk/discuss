b5a9236e73740b5694077c8b422faa5ac9d1509f
Marcel Horlings <marcel.horlings@gmail.com>
2017-02-07 20:51:17 +0100

    update topics

--- web/router.ex
+++ web/router.ex
@@ -16,10 +16,12 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-    get "/topics", TopicController, :index
+    # get "/", PageController, :index
+    get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    put "/topics/:id", TopicController, :update
+    get "/topics/:id/edit", TopicController, :edit
   end
 
   # Other scopes may use custom stacks.
